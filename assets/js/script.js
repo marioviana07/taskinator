88f8528cd03b9b8e9e4b36d0012befdc56dadf72
marioviana07 <mario_viana_2005@yahoo.com>
2020-06-09 14:23:23 -0700

    handler

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // buttonEl.addEventListener("click", function() {
@@ -8,10 +8,14 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 //   tasksToDoEl.appendChild(listItemEl);
 // });
 
-var createTaskHandler = function() {
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-  }
-  buttonEl.addEventListener("click", createTaskHandler);
+var createTaskHandler = function (event) {
+  
+  event.preventDefault();
+  
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task.";
+  tasksToDoEl.appendChild(listItemEl);
+};
+
+formEl.addEventListener("submit", createTaskHandler);
